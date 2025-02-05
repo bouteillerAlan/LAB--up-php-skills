a73dd147f20719e190c50b7a03ae5148530c7bd4
Bouteiller [a2n] Alan <a2n.dev@pm.me>
2025-02-05 13:47:40 +0100

    improving the structure

--- www/contact.php
+++ www/contact.php
@@ -2,6 +2,7 @@
 
 $_MAXFILESIZE = 1000000;
 $_FILEEXTALLOWED = ["jpg", "jpeg", "gif", "png"];
+$_FILESTORAGEPATH = __DIR__ . "/uploads/";
 
 // cleaning up the data before anything else
 // I used strip_tag, but we totally can use htmlspecialchars
@@ -55,28 +56,21 @@ elseif (!isValueEmail($cleanedGetData["email"]))
 // if everything is ok
 echo "Welcome " . $cleanedGetData["name"] . ", your email is " . $cleanedGetData["email"];
 
-// is file defined
-if (!isValueSet($_FILES["file"]))
-{
-    echo "You have included no file";
-    return;
-}
-
-// does the file up have error
-if ($_FILES["file"]["error"] !== 0)
+// check the data
+if (!isValueSet($_FILES["file"]) || $_FILES["file"]["error"] !== 0)
 {
     echo "The file upload have encounter a problem (error code: {$_FILES["file"]["error"]})";
     return;
 }
 
-// checking the size (1Mo max ~)
+// check the size
 if ($_FILES["file"]["size"] > $_MAXFILESIZE)
 {
     echo "The file is too heavy, you need to send a smaller file (less than " . $_MAXFILESIZE / 1000000 . "Mo)";
     return;
 }
 
-// checking the ext
+// checking the extension
 $fileInfos = pathinfo($_FILES["file"]["name"]);
 if (!array_search($fileInfos["extension"], $_FILEEXTALLOWED))
 {
@@ -84,3 +78,9 @@ if (!array_search($fileInfos["extension"], $_FILEEXTALLOWED))
     return;
 }
 
+// check if the storage file exist
+if (!is_dir($_FILESTORAGEPATH))
+{
+    echo "Hu ho we have encountered a problem on the server, contact the support!";
+    return;
+}
