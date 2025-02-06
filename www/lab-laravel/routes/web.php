bbe5b66ce33a89471e5d71863d3938c05353b962
Bouteiller [a2n] Alan <a2n.dev@pm.me>
2025-02-06 22:54:13 +0100

    update nginx config and start routing

--- www/lab-laravel/routes/web.php
+++ www/lab-laravel/routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix("blog")->name("blog.")->group(function () {
+    Route::get('/', function (Request $request) {
+        return [
+            "link" => \route('blog.show', ["slug" => "article", "id" => 13])
+        ];
+    })->name("index");
+
+    Route::get('/{slug}-{id}', function (string $slug, string $id) {
+        return [
+            "id" => $id,
+            "slug" => $slug
+        ];
+    })->where([
+        "id" => "[0-9]+",
+        "slug" => "[a-z0-9\-]*"
+    ])->name("show");
 });
