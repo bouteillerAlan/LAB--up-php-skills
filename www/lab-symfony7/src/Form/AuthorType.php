f7ae5a63c289fafa4518d28bf7a1e14a526d485a
Bouteiller [a2n] Alan <a2n.dev@pm.me>
2025-02-08 21:02:10 +0100

    add form and form theming

--- www/lab-symfony7/src/Form/AuthorType.php
+++ www/lab-symfony7/src/Form/AuthorType.php
@@ -4,8 +4,8 @@ namespace App\Form;
 
 use App\Entity\Author;
 use App\Entity\Book;
-use Doctrine\DBAL\Types\DateType;
-use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -16,24 +16,30 @@ class AuthorType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class)
+            ->add('name', TextType::class, [
+                'label' => 'Nom de l\'auteur'
+            ])
             ->add('dateOfBirth', DateType::class, [
                 'input' => 'datetime_immutable',
                 'widget' => 'single_text',
+                'label' => 'Date de naissance'
             ])
             ->add('dateOfDeath', DateType::class, [
                 'input' => 'datetime_immutable',
                 'widget' => 'single_text',
-                'required' => false
+                'required' => false,
+                'label' => 'Date de mort'
             ])
             ->add('nationality', TextType::class, [
-                'required' => false
+                'required' => false,
+                'label' => 'Nationalité'
             ])
             ->add('books', EntityType::class, [
                 'class' => Book::class,
                 'choice_label' => 'id',
                 'multiple' => true,
-                'required' => false
+                'required' => false,
+                'label' => 'Livres'
             ])
         ;
     }
