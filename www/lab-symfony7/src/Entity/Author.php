96008e6608a7658df9db04fb1c43954d3cbd2bee
Bouteiller [a2n] Alan <a2n.dev@pm.me>
2025-02-09 16:58:18 +0100

    add some validation just for testing

--- www/lab-symfony7/src/Entity/Author.php
+++ www/lab-symfony7/src/Entity/Author.php
@@ -5,7 +5,9 @@ namespace App\Entity;
 use App\Repository\AuthorRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: AuthorRepository::class)]
 class Author
@@ -15,13 +17,17 @@ class Author
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Assert\Length(min: 10)]
+    #[Assert\NotBlank()]
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
-    #[ORM\Column(nullable: true)]
+    #[Assert\NotBlank()]
+    #[ORM\Column(type: Types::DATE_IMMUTABLE, nullable: true)]
     private ?\DateTimeImmutable $dateOfBirth = null;
 
-    #[ORM\Column(nullable: true)]
+    #[Assert\GreaterThan(propertyPath: 'dateOfBirth')]
+    #[ORM\Column(type: Types::DATE_IMMUTABLE, nullable: true)]
     private ?\DateTimeImmutable $dateOfDeath = null;
 
     #[ORM\Column(length: 255, nullable: true)]
