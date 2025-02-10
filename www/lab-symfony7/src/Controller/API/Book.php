631faa6da064c8c5cb01521e0ccb83ba65a28bb0
Bouteiller [a2n] Alan <a2n.dev@pm.me>
2025-02-10 14:52:59 +0100

    improve type

--- www/lab-symfony7/src/Controller/API/Book.php
+++ www/lab-symfony7/src/Controller/API/Book.php
@@ -4,13 +4,14 @@ namespace App\Controller\API;
 
 use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route(path: '/api')]
 class Book extends AbstractController
 {
     #[Route(path: '/books', name: 'api_book')]
-    public function getAll(BookRepository $bookRepository)
+    public function getAll(BookRepository $bookRepository): JsonResponse
     {
         return $this->json($bookRepository->findAll(), 200, [], ['groups' => ['book.index', 'book.details']]);
     }
